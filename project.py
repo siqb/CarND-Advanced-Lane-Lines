c02bc2c41a4a9a6f60bf00ca0247f245a8d08dbf
siqb <iqbals84@gmail.com>
2018-06-21 01:12:41 -0700

    Cleanup find_lines_in_margins().

--- project.py
+++ project.py
@@ -349,8 +349,7 @@ class Line():
         self.recent_xfitted.append(line_fitx)
         return line_fit, out_img
     
-    #def find_lines_in_margin(self, binary_warped, left_fit, right_fit):
-    def find_lines_in_margin(self, binary_warped, left_fit):
+    def find_lines_in_margin(self, binary_warped, line_fit):
         # Assume you now have a new warped binary image 
         # from the next frame of video (also called "binary_warped")
         # It's now much easier to find line pixels!
@@ -358,61 +357,48 @@ class Line():
         nonzeroy = np.array(nonzero[0])
         nonzerox = np.array(nonzero[1])
         margin = 100
-        left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + 
-        left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + 
-        left_fit[1]*nonzeroy + left_fit[2] + margin))) 
+        line_lane_inds = ((nonzerox > (line_fit[0]*(nonzeroy**2) + line_fit[1]*nonzeroy + 
+        line_fit[2] - margin)) & (nonzerox < (line_fit[0]*(nonzeroy**2) + 
+        line_fit[1]*nonzeroy + line_fit[2] + margin))) 
         
-        #right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + 
-        #right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + 
-        #right_fit[1]*nonzeroy + right_fit[2] + margin)))  
-        
-        # Again, extract left and right line pixel positions
-        leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds] 
-        #rightx = nonzerox[right_lane_inds]
-        #righty = nonzeroy[right_lane_inds]
+        # Again, extract lane line pixel positions
+        linex = nonzerox[line_lane_inds]
+        liney = nonzeroy[line_lane_inds] 
+
         # Fit a second order polynomial to each
-        left_fit = np.polyfit(lefty, leftx, 2)
-        #right_fit = np.polyfit(righty, rightx, 2)
+        line_fit = np.polyfit(liney, linex, 2)
+
         # Generate x and y values for plotting
         ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-        #right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+        line_fitx = line_fit[0]*ploty**2 + line_fit[1]*ploty + line_fit[2]
         
         # Create an image to draw on and an image to show the selection window
         out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
         window_img = np.zeros_like(out_img)
-        # Color in left and right line pixels
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-        #out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+        # Color in lane line pixels
+        out_img[nonzeroy[line_lane_inds], nonzerox[line_lane_inds]] = [255, 0, 0]
         
         # Generate a polygon to illustrate the search window area
         # And recast the x and y points into usable format for cv2.fillPoly()
-        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
-        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, 
+        line_line_window1 = np.array([np.transpose(np.vstack([line_fitx-margin, ploty]))])
+        line_line_window2 = np.array([np.flipud(np.transpose(np.vstack([line_fitx+margin, 
                                       ploty])))])
-        left_line_pts = np.hstack((left_line_window1, left_line_window2))
-        #right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
-        #right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, 
-        #                              ploty])))])
-        #right_line_pts = np.hstack((right_line_window1, right_line_window2))
+        line_line_pts = np.hstack((line_line_window1, line_line_window2))
         
         # Draw the lane onto the warped blank image
-        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
-        #cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+        cv2.fillPoly(window_img, np.int_([line_line_pts]), (0,255, 0))
         result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
         for i in range(0,720):
-            result[i][int(round(left_fitx[i]))]=[100,100,100]
+            result[i][int(round(line_fitx[i]))]=[100,100,100]
         #plt.imshow(result)
-        #plt.plot(left_fitx, ploty, color='yellow')
-        #plt.plot(right_fitx, ploty, color='yellow')
+        #plt.plot(line_fitx, ploty, color='yellow')
         #plt.xlim(0, 1280)
         #plt.ylim(720, 0)
 
-        self.allx = leftx
-        self.ally = lefty
+        self.allx = linex
+        self.ally = liney
 
-        #self.save_current_fit(left_fit, left_fitx)
+        #self.save_current_fit(line_fit, line_fitx)
         #self.calculate_average_fit()
         return result
 
